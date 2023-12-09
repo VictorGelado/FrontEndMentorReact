09348b1e003dea5e425300e52f7f5875e47a3a81
Victor Manoel <victorgeladogithub@gmail.com>
2023-12-09 13:54:33 -0300

    init project

---
+++ src/data/var.js
@@ -0,0 +1,14 @@
+const Colors = {
+    white: 'hsl(0, 0%, 100%)',
+    lightPink: 'hsl(275, 100%, 97%)',
+    grayishPurple: 'hsl(292, 16%, 49%)',
+    darkPurple: 'hsl(292, 42%, 14%'
+};
+/*
+- White: hsl(0, 0%, 100%)
+- Light pink: hsl(275, 100%, 97%)
+- Grayish purple: hsl(292, 16%, 49%)
+- Dark purple: hsl(292, 42%, 14%)*/
+
+
+export default Colors;
