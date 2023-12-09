2baa969b941adb51849f40eb3de80cf1f8d4b91f
Victor Manoel <victorgeladogithub@gmail.com>
2023-12-09 15:21:45 -0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Card from './components/Card';
+import styled from 'styled-components';
+import Colors from './data/var';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${Colors.lightPink};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>awreawerw</h1>
-    </div>
+    <Container>
+      <Card/>
+    </Container>
   );
 }
 
--- src/data/var.js
+++ src/data/var.js
@@ -1,8 +1,7 @@
 const Colors = {
-    white: 'hsl(0, 0%, 100%)',
     lightPink: 'hsl(275, 100%, 97%)',
     grayishPurple: 'hsl(292, 16%, 49%)',
-    darkPurple: 'hsl(292, 42%, 14%'
+    darkPurple: 'hsl(292, 42%, 14%)'
 };
 /*
 - White: hsl(0, 0%, 100%)
