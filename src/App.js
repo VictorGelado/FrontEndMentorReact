f9e0b365a3ee210a6e4bf78b0a3e63f7d8e271cf
Victor Manoel <victorgeladogithub@gmail.com>
2023-12-16 22:11:20 -0300

    add attribution

--- src/App.js
+++ src/App.js
@@ -8,18 +8,15 @@ import backgroundDesktop from './assets/images/background-pattern-desktop.svg'
 const Container = styled.div`
   width: 100vw;
   min-height: 100vh;
-  padding: 30px;
+  padding: 30px 0;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background-color: ${Colors.lightPink};
   position: relative;
-
-  
-`;/*background-image: url(${backgroundDesktop});
-background-repeat: no-repeat;
-bakcground-position: top;
-bakcground-size: cover;*/
+  gap: 15px;  
+`
 
 const BackgroundImg = styled.img`
   min-width: 100%;
@@ -27,11 +24,35 @@ const BackgroundImg = styled.img`
   top: 0;
 `;
 
+const Attribution = styled.p`
+  font-size: 1.2rem;
+  color: ${Colors.darkPurple};
+  font-weight: 600;
+`;
+
+const A = styled.a`
+  text-decoration: none;
+  color: ${Colors.grayishPurple};
+
+  &:hover {
+    & {
+      color: ${Colors.darkPurple};
+      transition: color 0.20s ease-in;
+    }
+  }
+`;
+
 function App() {
   return (
     <Container>
       <BackgroundImg src={backgroundDesktop} alt="Background Image"/>
+
       <Card />
+
+      <Attribution>
+        Challenge by <A href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</A>. 
+        Coded by <A href="https://github.com/VictorGelado/" target="_blank">Victor Gelado</A>.
+      </Attribution>
     </Container>
   );
 }
